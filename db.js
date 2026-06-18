3f1456b5eb8f0a0a80eeb0a0b35c0b172fe0cbf5
lsiddhi281-tech <lsiddhi281@gmail.com>
2026-06-18 16:03:48 +0530

    Update db.js

--- db.js
+++ db.js
@@ -2,10 +2,10 @@ var mysql = require('mysql2');
 var util = require('util');
 
 var conn = mysql.createConnection({
-    host:'localhost',
-    user:'root',
-    password:'root',
-    database:'dental'
+    host:'bdrmjqabafasj7rlnpa0-mysql.services.clever-cloud.com',
+    user:'urbxtuqvs76ix1ds',
+    password:'Xs1LMo0VIpr6hCLjtlxh',
+    database:'bdrmjqabafasj7rlnpa0'
 })
 
 var exe = util.promisify(conn.query).bind(conn);
